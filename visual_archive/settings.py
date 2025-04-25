1f8760660c5b4596784fa2f24aad0f1b28b7fd74
Can Dulundu <candulundu@gmail.com>
2025-04-26 00:28:58 +0300

    v1.1(Edit-Mode)

--- visual_archive/settings.py
+++ visual_archive/settings.py
@@ -79,12 +79,24 @@ WSGI_APPLICATION = 'visual_archive.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/5.2/ref/settings/#databases
 
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+import dj_database_url
+import os
+
+# Use PostgreSQL in production (Render) and SQLite in development
+if 'DATABASE_URL' in os.environ:
+    DATABASES = {
+        'default': dj_database_url.config(
+            conn_max_age=600,
+            conn_health_checks=True,
+        )
     }
-}
+else:
+    DATABASES = {
+        'default': {
+            'ENGINE': 'django.db.backends.sqlite3',
+            'NAME': BASE_DIR / 'db.sqlite3',
+        }
+    }
 
 
 # Password validation
